3067d253810ed0831f2e7234ccc263cced5db1ab
devopsgit1998 <163008208+devopsgit1998@users.noreply.github.com>
2025-08-02 12:00:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "random_string" "suffix" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.8.5"
+  version = "21.0.6"
 
   cluster_name    = local.cluster_name
   cluster_version = "1.29"
